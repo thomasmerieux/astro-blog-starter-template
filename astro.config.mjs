d3789e4e59e525752b9b449adb9146fabeeb0b80
thomas <thomas@thomass-MacBook-Air.local>
2025-08-10 11:08:06 +0300

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,21 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import tailwind from '@astrojs/tailwind';
 
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), tailwind()],
   adapter: cloudflare({
     platformProxy: {
       enabled: true,
     },
   }),
+    i18n: {
+        defaultLocale: "en",
+        locales: ["en", "fr", "ro"]
+    }
 });
